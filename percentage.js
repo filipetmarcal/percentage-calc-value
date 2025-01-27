c4c50faddb0772f0d117eb256d81dbfa9c87f801
filipe04developing <76597608+filipe04developing@users.noreply.github.com>
2025-01-27 12:51:36 -0300

    Create percentage.js

---
+++ percentage.js
@@ -0,0 +1,15 @@
+/*
+This function use the percentage value and convert to decimal number.
+How to use: Multiply the value to percent. Example: 50 * percent(30)
+*/
+function percent(float value){
+  return value / 100;
+}
+
+/*
+This function convert any decimal number to percentage.
+How to use: With any decimal value, use dec2percentage(value).
+*/
+function dectopercent(float value){
+  return value * 100;
+}
