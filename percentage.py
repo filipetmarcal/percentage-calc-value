2d2d546aed0705d5945c99ae60e2214f9084db29
Filipe Marçal <76597608+filipetmarcal@users.noreply.github.com>
2025-01-30 20:26:01 -0300

    Update percentage.py

--- percentage.py
+++ percentage.py
@@ -1,14 +1,28 @@
-#This function use the percentage value and convert to decimal number.
-#How to use: Multiply the value to percent. Example: 50 * percent(30)
+'''
+This function use the percentage 
+value and convert to decimal number.
+How to use: Multiply the value to 
+percent. Example: 50 * percent(30)
+'''
 def percent(value):
   return value / 100
-
-#This function convert any decimal number to percentage.
-#How to use: With any decimal value, use dec2percentage(value). Example: dec2percent(0.5)
+  
+'''
+This function convert any decimal 
+number to percentage.
+How to use: With any decimal value, 
+use dec2percentage(value). 
+Example: dec2percent(0.5)
+'''
 def dec2percent(value):
   return value * 100
-
-#This function convert any decimal number to percentage and print "{value}%".
-#How to use: With any decimal value, use dectopercentage(value). Example: dectopercent(0.75)
+  
+'''
+This function convert any decimal number 
+to percentage and print "{value}%".
+How to use: With any decimal value, use 
+dectopercentage(value). 
+Example: dectopercent(0.75)
+'''
 def dectopercent(value):
   return str(value * 100) + '%'
